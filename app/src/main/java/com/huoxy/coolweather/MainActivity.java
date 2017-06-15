1e4e09a17dd158496510285369175549df5823ee
huoxiaoyi <huoxy@huoxy-MBP.local>
2017-06-15 19:42:24 +0800

    git reset test

--- app/src/main/java/com/huoxy/coolweather/MainActivity.java
+++ app/src/main/java/com/huoxy/coolweather/MainActivity.java
@@ -1,7 +1,9 @@
 package com.huoxy.coolweather;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "HaHa", Toast.LENGTH_SHORT).show();
+            }
+        }, 2000);
     }
 
     @Override
